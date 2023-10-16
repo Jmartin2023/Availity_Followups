8ff7ec6f556cf25cd799ca4044ec9f6840183038
Muhammad Danyal Aman <mdanyal@ad.mcmso.co>
2023-10-16 12:41:37 +0500

    first commit of Availity

---
+++ src/test/java/rpa/Availity_Followup.java
@@ -0,0 +1,597 @@
+package rpa;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Locale;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.SkipException;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.xml.sax.SAXException;
+
+import objects.ExcelOperations;
+import objects.SeleniumUtils;
+import objects.Utility;
+import utilities.ExcelReader;
+
+
+
+
+public class Availity_Followup {
+	Logger logger = LogManager.getLogger(Availity_Followup.class);
+
+	String projDirPath, status, claimNo ,claimNumAvaility, DOB ,serviceDate ,firstName, lastName,memberID,ecwStatus,DOS, claimStatus,dateofbirth, npivalue, charges,currency, error, originalTab;
+	
+	SimpleDateFormat parser = new SimpleDateFormat("MM/dd/yy");
+
+	SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+
+	public static ExcelReader excel; 
+	public static String sheetName = "Sheet1";
+	int rowNum = 1;
+
+	WebDriver driver;
+
+	//JavascriptExecutor js;
+	SeleniumUtils sel;
+	Utility utility;
+
+	ExcelOperations excelFile;
+	Availity_Objects bcbs;
+	static String excelFileName;
+
+	@BeforeTest
+	public void preRec() throws InterruptedException, SAXException, IOException, ParserConfigurationException {
+
+		sel = new SeleniumUtils(projDirPath);
+
+		driver = sel.getDriver();
+
+		//js = (JavascriptExecutor) driver;
+		bcbs= new Availity_Objects(driver);
+		utility = new Utility();
+
+
+		String[] params = new String[]{"url", "username", "password", "state","payer","excelName"};
+		HashMap<String, String> configs = utility.getConfig("config.xml", params);
+
+		String url = configs.get("url"), 
+				username = configs.get("username"), 
+				state = configs.get("state"),
+				payer = configs.get("payer"),
+				password = configs.get("password");
+
+		excelFileName = configs.get("excelName");
+		System.out.println(excelFileName);
+
+		driver.get(url);
+		logger.info("Open url: " + url);
+
+		sel.pauseClick(bcbs.loginBtn, 10);
+
+		bcbs.usernameField.sendKeys(username);
+		logger.info("Enter username: " + username);
+
+
+		bcbs.passwordField.sendKeys(password);
+		logger.info("Enter password");
+
+		
+
+		bcbs.loginBtn.click();
+		logger.info("Click login button");
+
+		Thread.sleep(5000);
+		try {
+		bcbs.waitFunc(bcbs.downArrowDrpDwn);
+		bcbs.downArrowDrpDwn.click();
+		logger.info("Clicked on down arrow dropdown");
+		}catch(Exception e) {
+			
+				for(int i=0; i<5; i++) {
+					Thread.sleep(4000);
+				try{ 
+					if(bcbs.downArrowDrpDwn.isDisplayed()) 
+					{
+					bcbs.waitFunc(bcbs.downArrowDrpDwn);
+					bcbs.downArrowDrpDwn.click();
+					logger.info("Clicked on down arrow dropdown");
+					break;
+					}
+					}catch(Exception e1) {}
+			}
+		}
+
+		bcbs.waitFunc(bcbs.stateInput);
+		bcbs.stateInput.sendKeys(state);
+		Thread.sleep(2000);
+		//bcbs.michiganState.click();;
+		bcbs.getState(state).click();
+		logger.info("State enetered as"+ state);
+		
+		Thread.sleep(3000);
+		driver.switchTo().frame("newBodyFrame");
+		sel.pauseClick(bcbs.claimStatusTab,60);
+		bcbs.waitFunc(bcbs.claimStatusTab);
+		
+		bcbs.claimStatusTab.click();
+		logger.info("Clicked on Claim Status tab");
+		
+		Thread.sleep(2000);
+		try {
+		bcbs.waitFunc(bcbs.organizationInput);
+		bcbs.organizationInput.clear();
+		}catch(Exception e) {
+			for(int i=0; i<5; i++) {
+				Thread.sleep(4000);
+			if(bcbs.organizationInput.isDisplayed()) {
+				break;
+			}
+		}
+		}
+	
+		bcbs.organizationInput.sendKeys(Keys.ENTER);
+		logger.info("Entered Organization: ARK Laboratory LLC");
+		 
+
+		bcbs.payerInput.clear();
+		bcbs.payerInput.sendKeys(payer);
+		Thread.sleep(1000);
+		bcbs.payerInput.sendKeys(Keys.ENTER);
+		bcbs.payerInput.sendKeys(Keys.SPACE);
+		logger.info("Entered Payer: "+payer);
+		
+		Thread.sleep(3000);
+		bcbs.waitFunc(bcbs.hipaaTab);
+		bcbs.hipaaTab.click();
+		logger.info("Clicked on HIPAA tab");
+		originalTab  = driver.getWindowHandle();
+		System.out.println(driver.getWindowHandle());
+	}
+
+	@Test(dataProvider= "getData") 
+	public void AvailityPortal(Hashtable<String,String> data) throws InterruptedException, ParseException {
+		rowNum++;
+		
+		status = data.get("Final Status");
+		
+		if(status.isBlank() || status.isBlank()) {
+			bcbs.clearForm.click();
+			  
+			SimpleDateFormat parser = new SimpleDateFormat("MM/dd/yy");
+			// output format: yyyy-MM-dd
+			SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+			DOB = data.get("Date of Birth");
+			DOS = data.get("Service Date");
+			//System.out.println(formatter.format(parser.parse(DOB)) +" is date");
+			DOS= formatter.format(parser.parse(DOS));
+			dateofbirth=	formatter.format(parser.parse(DOB));
+			firstName = data.get("First Name").toUpperCase().trim();
+			lastName = data.get("Last Name").toUpperCase().trim();
+			memberID = data.get("Member ID");
+			charges = data.get("Billed Charge").replace("$", "");
+			double balanceDouble = Double.parseDouble(charges); 
+			 NumberFormat currencyformatter=NumberFormat.getCurrencyInstance(Locale.US);  
+			  currency=currencyformatter.format(balanceDouble);
+			System.out.println(currency);
+			logger.info("Last name is "+lastName);
+			logger.info("First name is "+firstName);
+			
+			
+			try {
+				bcbs.waitFunc(bcbs.providerNpi);
+				}catch(Exception e) {
+					for(int i=0; i<5; i++) {
+						Thread.sleep(4000);
+					if(bcbs.providerNpi.isDisplayed()) {
+						break;
+					}
+				}
+				}
+			 ///////////////////////////////////////
+			bcbs.providerNpi.clear();
+			for(int i=0; i<5; i++) {
+				npivalue= bcbs.providerNpi.getAttribute("value");
+				if(npivalue.isBlank() || npivalue.isEmpty()) {
+				bcbs.providerNpi.sendKeys("1700120938");
+				logger.info("Entered NPI: 1700120938");
+				}
+				else {
+					bcbs.providerNpi.clear();
+				}
+			}
+			
+			
+			bcbs.waitFunc(bcbs.memberIDInput); 
+			bcbs.memberIDInput.clear();
+			bcbs.memberIDInput.sendKeys(memberID);
+			logger.info("Entered member ID: "+ memberID);
+			
+			bcbs.patientLastName.clear();
+			bcbs.patientLastName.sendKeys(lastName);
+			logger.info("Entered last Name: "+ lastName);
+			
+			bcbs.patientFirstName.clear();
+			bcbs.patientFirstName.sendKeys(firstName);
+			logger.info("Entered first Name: "+ firstName);
+			
+			bcbs.patientBirthDate.clear();
+			bcbs.patientBirthDate.sendKeys(dateofbirth);
+			logger.info("DOB entered as: "+ dateofbirth);
+			
+			bcbs.serviceDatestart.clear();
+			bcbs.serviceDatestart.sendKeys(DOS);
+		//	bcbs.serviceDatestart.sendKeys(Keys.ENTER);
+			logger.info("DOS entered as: "+ DOS);
+			
+			bcbs.serviceDateend.clear();
+			bcbs.serviceDateend.sendKeys(DOS);
+		//	bcbs.serviceDateend.sendKeys(Keys.ENTER);
+			logger.info("DOS entered as: "+ DOS);
+			bcbs.claimAmountInput.click();
+			
+			bcbs.submitBtn.click();
+			logger.info("Clicked on Submit button");
+			
+			bcbs.waitFunc(bcbs.transactionIDLogo);
+			
+			try {
+				bcbs.waitFunc(driver.findElement(By.xpath("//div[1][@role='alert']/ul/li")));
+				driver.findElement(By.xpath("//div[1][@role='alert']/ul/li")).isDisplayed();
+				error=driver.findElement(By.xpath("//div[1][@role='alert']/ul/li")).getText();
+				bcbs.clearForm.click();
+				logger.info("Form cleared");
+			
+				excel.setCellData(sheetName, "Final Status", rowNum, error);
+				throw new SkipException("Skipping this exception, "+error);
+				
+			}catch(Exception e) {
+				
+			}
+			driver.switchTo().defaultContent();
+			driver.switchTo().frame("newBody");
+			try {
+				bcbs.waitFunc(bcbs.claimStatus(firstName, lastName, memberID,currency));
+				}catch(Exception e) {
+					for(int i=0; i<5; i++) {
+						Thread.sleep(4000);
+					try {
+							bcbs.claimStatus(firstName, lastName, memberID,currency).isDisplayed();
+						break;
+					}catch(Exception e1) {}	
+				}
+					
+				}
+			
+		try {	
+			claimStatus= bcbs.claimStatus(firstName, lastName, memberID,currency).getText();
+		}catch(Exception e) {
+			excel.setCellData(sheetName, "Final Status", rowNum, "member Id, name or charge mismatch");
+			throw new SkipException("Skipping this exception, member Id, name or charge mismatch");
+		}
+			
+			logger.info(claimStatus);
+			
+			if(claimStatus.equals("FINALIZED")) {
+				
+				bcbs.claimStatus(firstName, lastName, memberID,currency).click();
+				logger.info("Clicked on the claim status");
+				
+				
+				try {
+					bcbs.waitFunc(bcbs.claimNumber);
+					}catch(Exception e) {
+						for(int i=0; i<5; i++) {
+							Thread.sleep(4000);
+						if(bcbs.claimNumber.isDisplayed()) {
+							break;
+						}
+					}
+					}
+				
+				claimNumAvaility= 	bcbs.claimNumber.getText();
+				logger.info("Claim number in availity app is "+ claimNumAvaility);
+				bcbs.remittanceBtn.click();
+				logger.info("Clicked on remittance button");
+		/*		
+				ArrayList<String> windowTabs = new ArrayList<String>(driver.getWindowHandles());
+				for(String str: windowTabs) {
+					if(!str.equals(originalTab)) {
+						driver.switchTo().window(tabs.get(0))
+					}
+				}
+				*/
+				
+				Thread.sleep(10000);
+				ArrayList<String> newTb = new ArrayList<String>(driver.getWindowHandles());
+			      //switch to new tab
+			      driver.switchTo().window(newTb.get(1));
+			      driver.switchTo().frame("newBodyFrame");
+			     // System.out.println("Page title of new tab: " + driver.getTitle());
+			      //switch to parent window
+			    //  driver.switchTo().window(newTb.get(0));
+				
+				
+				try {
+					bcbs.waitFunc(bcbs.closePopUp);
+					bcbs.closePopUp.click();
+					logger.info("Remittannce pop up closed");
+				}catch(Exception e) {}
+				
+				//change tab ghere
+				bcbs.waitFunc(bcbs.claimTab);
+				bcbs.claimTab.click();
+				logger.info("Clicked on claim tab");
+				
+				bcbs.claimSearchInput.sendKeys(claimNumAvaility);
+				bcbs.claimSearchButton.click();
+				logger.info("Clicked on Search button");
+				try {
+					
+					bcbs.waitFunc(bcbs.downloadEOB(firstName, lastName, currency));
+					logger.info("Download EOB button found in try");
+				}catch(Exception e) {
+					for(int i=0; i<5; i++) {
+						Thread.sleep(6000);
+					try {	
+					if(bcbs.downloadEOB(firstName, lastName, currency).isDisplayed()) {
+						logger.info("Download EOB button found in catch");
+						break;
+					}
+					}catch(Exception e1) {}
+				}
+				}
+				
+				try {
+					driver.findElement(By.xpath("//strong[contains(text(),\"We didn't find any remits to show\")]")).isDisplayed();
+					excel.setCellData(sheetName, "Claim Status", rowNum, "FINALIZED + We didn't find any remits to show");
+					driver.close();
+					driver.switchTo().window(newTb.get(0));
+					driver.switchTo().frame("newBodyFrame");
+					throw new SkipException("Skipping this exception, We didn't find any remits to show");
+					
+				}catch(Exception e) {
+					
+				}
+				
+				
+				bcbs.downloadEOB(firstName, lastName, currency).click();
+				logger.info("Download EOB clicked first time");
+				try {
+					bcbs.waitFunc(driver.findElement(By.xpath("//h3[contains(text(),'EOP/EOB Downloads')]")));
+				}catch(Exception e) {
+					
+					for(int i=0; i<5; i++) {
+						
+						bcbs.downloadEOB(firstName, lastName, currency).click();
+						Thread.sleep(5000);
+					try {
+					if(driver.findElement(By.xpath("//h3[contains(text(),'EOP/EOB Downloads')]")).isDisplayed()){
+						logger.info("Dialogue box for downloads opened");
+						break;}
+					}catch(Exception e1) {}
+						
+				}
+				}
+				try {
+					driver.findElement(By.xpath("//label[contains(text(),'Auto Downloading')]")).isDisplayed();
+					logger.info("Auto Downloading Dialogue box displayed");
+					
+					}catch(Exception e2) {
+						
+						
+						for(int i=0; i<5; i++) {
+
+							Thread.sleep(5000);
+						try {
+						if(driver.findElement(By.xpath("//label[contains(text(),'Auto Downloading')]")).isDisplayed()){
+							logger.info("Auto Downloading Dialogue box displayed");
+							break;
+							}
+						}catch(Exception e1) {}
+					}
+			
+				}
+				
+				Thread.sleep(4000);
+				logger.info("Download button clicked for patient with charges as "+currency);
+				driver.close();
+				driver.switchTo().window(newTb.get(0));
+				driver.switchTo().frame("newBodyFrame");
+				
+				try {
+					bcbs.waitFunc(bcbs.resultsTab);
+					bcbs.resultsTab.click();
+					logger.info("Clicked on results");
+				}catch(Exception e) {}
+				
+			}else {
+				
+			}
+			
+			excel.setCellData(sheetName, "Claim Status", rowNum, claimStatus);
+			excel.setCellData(sheetName, "Final Status", rowNum, "Pass");
+			bcbs.clearForm.click();
+			logger.info("Form cleared");
+			
+		}
+}
+	@Test(priority=3, dependsOnMethods="AvailityPortal") 
+	public void ecwLogin(){
+		rowNum = 1;
+		driver.get("https://azuarq3ezwcrczrn8xapp.ecwcloud.com/mobiledoc/jsp/webemr/login/newLogin.jsp#/mobiledoc/jsp/webemr/webpm/claimLookup.jsp");
+		logger.info("Open url: https://azuarq3ezwcrczrn8xapp.ecwcloud.com/mobiledoc/jsp/webemr/login/newLogin.jsp#/mobiledoc/jsp/webemr/webpm/claimLookup.jsp");
+		bcbs.waitFunc(bcbs.usernameFieldECW);
+		
+		bcbs.usernameFieldECW.clear();
+		bcbs.usernameFieldECW.sendKeys("jimmartin");
+		logger.info("Enter username: jimmartin");
+
+		bcbs.nextBtnECW.click();
+		logger.info("Click next button");
+
+		sel.pauseClick(bcbs.loginBtnECW,10);
+		
+		bcbs.passwordFieldECW.clear();
+		bcbs.passwordFieldECW.sendKeys("!Ndian@193");
+		logger.info("Enter password");
+
+		bcbs.loginBtnECW.click();
+		logger.info("Clicked on login");
+		
+		sel.pauseClick(bcbs.patientLookupBtnECW,150);
+		
+
+
+		bcbs.expandMenubtnECW.click();
+		logger.info("Click expand menu button");
+
+		bcbs.billingTabECW.click();
+		logger.info("Click Billing Tab");
+
+		bcbs.claimsMenuECW.click();
+		logger.info("Click Claims");
+		
+	}
+	@Test(dataProvider= "getData",priority=4, dependsOnMethods={"ecwLogin","AvailityPortal"}) 
+	public void ASRtoECWCase(Hashtable<String,String> data) throws InterruptedException, ParseException, IOException {
+		rowNum++;
+		status = data.get("Final Status");
+		ecwStatus = data.get("ECW Status");
+		claimStatus = data.get("Claim Status");
+
+		if(status.equals("Pass")&& (ecwStatus.isBlank() || ecwStatus.isEmpty()) ) {
+			
+			sel.pauseClick(bcbs.claimLookupInputECW, 30);
+			claimNo = data.get("CLAIMS#").replace(".0", "");
+			bcbs.claimLookupInputECW.clear();
+			bcbs.claimLookupInputECW.sendKeys(claimNo);
+			logger.info("Claim no entered as :"+ claimNo);
+			bcbs.claimLookupBtnECW.click();
+			logger.info("Clicked on look up button");
+			
+			sel.pauseClick(bcbs.saveClaimBtnECW, 50);
+			sel.pauseClick(bcbs.followUpArrowECW, 50);
+			
+			try {
+				if(!bcbs.claimNotesECW.isDisplayed()) {
+					bcbs.followUpArrowECW.click();
+					logger.info("Clicked on follow up arrows");
+				}
+				
+			}catch(Exception e) {
+				
+			}
+			sel.pauseClick(bcbs.claimNotesECW, 15);
+			Select select = new Select(driver.findElement(By.xpath("//select[contains(@id,'claimStatusSel')]")));
+			if(claimStatus.equals("FINALIZED")) {
+				bcbs.claimNotesECW.sendKeys("Claim is processed. EOB is downloaded.");
+				logger.info("Claim is processed. EOB is downloaded. Entered");
+				select.selectByVisibleText("Insurance Accepted");
+			}
+			else if(claimStatus.equals("PENDING")) {
+				bcbs.claimNotesECW.sendKeys("Claim is still in process.");
+				logger.info("Claim is still in process.");
+				select.selectByVisibleText("Medcare - In Process Claims");
+			}
+			else {
+				bcbs.claimNotesECW.sendKeys(claimStatus);
+				logger.info("Claim is"+claimStatus +" Entered");
+			}
+			
+			
+			
+			
+			
+			bcbs.saveClaimBtnECW.click();
+			logger.info("Save button clicked");
+			excel.setCellData(sheetName, "ECW Status", rowNum, "Pass");
+			
+	}
+	}
+	@AfterMethod()
+	public void afterMethod(ITestResult result) throws IOException {
+
+		if(!result.isSuccess()) {
+			// Test Failed
+			String error = result.getThrowable().getLocalizedMessage();
+			logger.info(error);
+			//result.getThrowable().printStackTrace();
+			try {
+				TakesScreenshot ts = (TakesScreenshot) driver;
+				File ss = ts.getScreenshotAs(OutputType.FILE);
+				String ssPath = "./Screenshots/" + result.getName() + " - " + rowNum + ".png";
+				FileUtils.copyFile(ss, new File(ssPath));
+			} catch (Exception e) {
+				System.out.println("Error taking screenshot");
+			}
+
+		}
+		else {
+			logger.info("Test completed successfully");
+		}}
+	@DataProvider
+	public static Object[][] getData(){
+
+
+		if(excel == null){
+
+
+			excel = new ExcelReader(System.getProperty("user.dir")+"\\"+excelFileName);
+
+
+		}
+
+
+		int rows = excel.getRowCount(sheetName);
+		int cols = excel.getColumnCount(sheetName);
+
+		Object[][] data = new Object[rows-1][1];
+
+		Hashtable<String,String> table = null;
+
+		for(int rowNum=2; rowNum<=rows; rowNum++){
+
+			table = new Hashtable<String,String>();
+
+			for(int colNum=0; colNum<cols; colNum++){
+
+				//	data[rowNum-2][colNum]=	excel.getCellData(sheetName, colNum, rowNum);
+
+				table.put(excel.getCellData(sheetName, colNum, 1), excel.getCellData(sheetName, colNum, rowNum));	
+				data[rowNum-2][0]=table;	
+
+			}
+		}
+
+		return data;
+
+	}}
---
+++ src/test/java/rpa/Availity_Objects.java
@@ -0,0 +1,252 @@
+package rpa;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Availity_Objects {
+
+	WebDriver driver;
+//	private WebDriverWait wait10, wait20;
+	private WebDriverWait wait;
+	public Availity_Objects(WebDriver driver) {
+		super();
+		this.driver = driver;
+		wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+	
+	@FindBy(id = "userId")
+	public WebElement usernameField;
+	
+	@FindBy(id = "password")
+	public WebElement passwordField;
+	
+	@FindBy(id = "loginFormSubmit")
+	public WebElement loginBtn;
+	
+	@FindBy(xpath = "//div[@class='av-large-logo']")
+	public WebElement availityLogo;
+	
+	@FindBy(xpath = "//h3[text()='Claim Status']")
+	public WebElement claimStatusTab;
+	
+	@FindBy(xpath = "//span[@id='select2-chosen-1']/following-sibling::span/b[@role='presentation']")
+	public WebElement downArrowDrpDwn;
+	
+	@FindBy(id = "s2id_autogen1_search")
+	public WebElement stateInput;
+	
+	@FindBy(xpath = "//div/span[text()='Michigan']")
+	public WebElement michiganState;
+	
+	public WebElement getState(String state) {
+		return driver.findElement(By.xpath("//div/span[text()='"+state+"']"));
+	}
+	
+	@FindBy(id = "organization")
+	public WebElement organizationInput;
+	
+	@FindBy(id = "payer")
+	public WebElement payerInput;
+	
+	@FindBy(id = "HIPAA Standard")
+	public WebElement hipaaTab;
+	
+	@FindBy(id = "patientMemberId")
+	public WebElement memberIDInput;
+	
+	
+	
+	
+	
+	
+	@FindBy(id = "patientLastName")
+	public WebElement patientLastName;
+	
+	@FindBy(id = "patientFirstName")
+	public WebElement patientFirstName;
+	
+	@FindBy(id = "patientBirthDate")
+	public WebElement patientBirthDate;
+	
+	@FindBy(id = "serviceDates-start")
+	public WebElement serviceDatestart;
+	
+	@FindBy(id = "serviceDates-end")
+	public WebElement serviceDateend;
+	
+	@FindBy(id = "submit-by276")
+	public WebElement submitBtn;
+	
+	@FindBy(id = "providerNpi")
+	public WebElement providerNpi;
+	
+	
+	@FindBy(xpath = "//table[@id='claimsTable']/descendant::td[contains(text(),'ADAMS, ELLERY')]/following-sibling::td[text()='MIJ892008138']/preceding-sibling::td[4]/span")
+	public WebElement claimStatus;
+	
+	public WebElement claimStatus(String firstname, String lastname, String memberid, String balance) {
+		return driver.findElement(By.xpath("//table[@id='claimsTable']/descendant::td[contains(text(),'"+lastname.toUpperCase()+", "+firstname.toUpperCase()+"')]/following-sibling::td[text()='"+memberid.toUpperCase()+"']/following-sibling::td[text()='"+balance+"']/preceding-sibling::td[7]/span"));
+	}
+	//table[@id='claimsTable']/descendant::td[contains(text(),'BINGHAM, EMERSYN')]/following-sibling::td[text()='XYQ892408913']/following-sibling::td[text()='$1,248.97']/preceding-sibling::td[7]/span
+	@FindBy(id = "claimAmount")
+	public WebElement claimAmountInput;
+	
+	@FindBy(id = "clearForm")
+	public WebElement clearForm;
+	
+	@FindBy(xpath = "//strong[contains(text(),'Transaction ID:')]")
+	public WebElement transactionIDLogo;
+	
+	@FindBy(xpath = "//p[text()='Claim Number']/following-sibling::p")
+	public WebElement claimNumber;
+	
+	@FindBy(id = "remittanceButton")
+	public WebElement remittanceBtn;
+	
+	@FindBy(xpath = "//button[@aria-label='Close']")
+	public WebElement closePopUp;
+	
+	@FindBy(xpath = "//span[text()='Claim']")
+	public WebElement claimTab;
+	
+	
+	@FindBy(id = "claimSearchInput")
+	public WebElement claimSearchInput;
+	
+	@FindBy(id = "claimSearchButton")
+	public WebElement claimSearchButton;
+	
+	@FindBy(xpath = "//a[text()='Results']")
+	public WebElement resultsTab;
+	
+	
+	
+	public WebElement downloadEOB(String firstname, String lastname, String balance) {
+		return driver.findElement(By.xpath("//div[contains(text(),'"+lastname.toUpperCase()+", "+firstname.toUpperCase()+"')]/ancestor::div[3]/following-sibling::div/span[contains(text(),'"+balance+"')]/parent::div/following-sibling::div/descendant::button[contains(@id,'claimeob')]"));
+	}
+	
+	////////////////////////////////
+
+	
+	@FindBy(id = "billingClaimBtn35")
+	public WebElement adjustmentBtn;
+	
+	@FindBy(id = "billingClaimBtn36")
+	public WebElement cancelBtn;
+	
+	
+	@FindBy(id = "claimAdjustmentsBtn9")
+	public WebElement addBtn;
+	
+	@FindBy(id = "claimAdjustmentsBtn11")
+	public WebElement threeDots;
+	
+	@FindBy(xpath = "//td[text()='No Authorization']")
+	public WebElement noAuthorization;
+	
+	@FindBy(id = "FinancialAdjustmentCodeBtn5")
+	public WebElement CodeOkBtn;
+	
+	@FindBy(id = "cptAmt")
+	public WebElement amountField;
+	
+
+	@FindBy(id = "claimAdjustmentsBtn13")
+	public WebElement AmountAddBtn;
+	
+	@FindBy(id = "claimAdjustmentsBtn14")
+	public WebElement postCPTBtn;
+	
+	@FindBy(xpath = "//div/span[@ng-bind='ClaimData.InvId']")
+	public WebElement ClaimVerif;
+	
+	@FindBy(id = "btnOKPostCPT")
+	public WebElement OkBtn2;
+	
+	@FindBy(xpath = "//table[@id='billingClaimTbl15']/descendant::td[text()='Payments/Adj']/following-sibling::td/span")
+	public WebElement adjustmentBalance;
+
+	@FindBy(id = "claimAdjustmentsBtn16")
+	public WebElement OkBtn3;
+	
+	@FindBy(xpath = "//button[contains(@id,'claimScreenOkBtn')]")
+	public WebElement OkBtn4;
+	
+	@FindBy(xpath = "//button[@id='claimAdjustmentsBtn20' and text()='Yes']")
+	public WebElement yesBtn;
+	
+	@FindBy(xpath = "//button[@id='claimAdjustmentsBtn23' and text()='Yes']")
+	public WebElement yesBtn2;
+	
+	
+	@FindBy(id = "doctorID")
+	public WebElement usernameFieldECW;
+	
+	@FindBy(id = "passwordField")
+	public WebElement passwordFieldECW;
+	
+
+	@FindBy(id = "Login")
+	public WebElement loginBtnECW;
+	
+	@FindBy(id = "nextStep")
+	public WebElement nextBtnECW;
+	
+	@FindBy(xpath = "//a[text()='Action' and @lookupshortcut]")
+	public WebElement patientLookupBtnECW;
+	
+	@FindBy(xpath = "//div[@class='favlist']/a[@class='navgator mainMenu']")
+	public WebElement expandMenubtnECW;
+	
+	@FindBy(xpath = "//li[@title='Billing']/a")
+	public WebElement billingTabECW;
+	
+	@FindBy(xpath = "//span[text()='Claims']/parent::a")
+	public WebElement claimsMenuECW;
+	
+	@FindBy(id = "claimLookupIpt10")
+	public WebElement claimLookupInputECW;
+	
+	@FindBy(id = "btnclaimlookup")
+	public WebElement claimLookupBtnECW;
+	
+	@FindBy(xpath = "//button[@ng-click='saveAllData()']")
+	public WebElement saveClaimBtnECW;
+	
+	
+	@FindBy(xpath = "//div[@class='billing-right-toggle claimRightPanel-tog']")
+	public WebElement followUpArrowECW;
+	
+	@FindBy(id = "claimRightPanelNotes")
+	public WebElement claimNotesECW;
+	
+	
+	public WebElement getbalanceFromApp(String date, String cpt) {
+		return driver.findElement(By.xpath("//tr/td[1][contains(text(),'"+date+"')]/following-sibling::td[2][contains(text(),'"+cpt+"')]/following-sibling::td[2]"));
+	}
+	
+	public WebElement writeOffbalanceInApp(String date, String cpt) {
+		return driver.findElement(By.xpath("//tr/td[1][contains(text(),'"+date+"')]/following-sibling::td[2][contains(text(),'"+cpt+"')]/following-sibling::td[3]/input"));
+	}
+	
+	public void waitFunc(WebElement webEle) {
+		wait.until(ExpectedConditions.elementToBeClickable(webEle));
+	}
+	
+	public void waitFuncInvisibility(WebElement webEle) {
+		wait.until(ExpectedConditions.invisibilityOf(webEle));
+	}
+	
+}
